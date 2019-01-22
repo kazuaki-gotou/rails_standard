8920ba14556d61521367d20bd65b81d14c84059a
GoTo_K <user@usernoMacBook-Pro.local>
2019-01-22 14:58:02 +0900

    rails15:create

--- claim/app/controllers/contacts_controller.rb
+++ claim/app/controllers/contacts_controller.rb
@@ -2,4 +2,7 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+  def create
+    Contact.create(name: params[:contact][:name], email: params[:contact][:email], content: params[:contact][:content])
+  end
 end
