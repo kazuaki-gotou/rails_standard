d305022324d781dab7be56ffb27ba6148db161ae
GoTo_K <user@usernoMacBook-Pro.local>
2019-01-22 14:39:55 +0900

    rails14

--- claim/config/routes.rb
+++ claim/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get '/contacts', to: 'contacts#new'
+  resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
